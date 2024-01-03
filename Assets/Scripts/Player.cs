05158e93602651d4222258de34033f62078a54aa
Heborine <seansilver562@gmail.com>
2024-01-02 21:31:49 -0800

    cleaning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,15 @@ public class Player : MonoBehaviour
     private float speed = 1.0f;
     [SerializeField]
     private float flapHeight = 10.0f;
-    [SerializeField]
-    public float gravityScaler = 1.0f;
 
     public Rigidbody2D rb;
     private float yVelocity = 0.0f;
     public bool gameOver = false;
-    
-
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        //rb.velocity = new Vector2(0, 0);
-        //rb.gravityScale = gravityScaler;
     }
 
     // Update is called once per frame
@@ -34,47 +28,22 @@ public class Player : MonoBehaviour
             {
                 yVelocity = flapHeight;
                 rb.velocity = new Vector2(speed, yVelocity);
-                //rb.rotation = 45f;
             }
             else if (Input.GetKeyDown("s") || Input.GetKeyDown(KeyCode.DownArrow))
             {
                 yVelocity = Mathf.Pow(yVelocity, 1.2f) * -1;
                 rb.velocity = new Vector2(speed, yVelocity);
-                //rb.rotation = -45f;
             }
-            rb.gravityScale = gravityScaler;
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        /*if (collision.gameObject.CompareTag("Finish"))
-        {
-            gameOver = true;
-            rb.velocity = new Vector2(0, 0);
-        }*/
+        // speed boost
         if (collision.gameObject.CompareTag("Finish"))
         {
             rb.velocity = new Vector2(rb.velocity.x + 1, rb.velocity.y);
-            //GameObject.FindWithTag("Player").GetComponent<Player>().rb.velocity = new Vector2(GameObject.FindWithTag("Player").GetComponent<Player>().rb.velocity.x + 1, GameObject.FindWithTag("Player").GetComponent<Player>().rb.velocity.y);
         }
-
-        /*
-        if (gameObject.tag == "Player")
-        {
-            if (collision.gameObject.CompareTag("Finish"))
-            {
-                gameOver = true;
-                rb.velocity = new Vector2(0, 0);
-            }
-        }
-        else if(gameObject.tag == "Border")
-        {
-            if (collision.gameObject.CompareTag("Finish"))
-            {
-                rb.velocity = new Vector2(rb.velocity.x + 1, rb.velocity.y + 1);
-            }
-        }*/
     }
 
 }
--- Assets/Scripts/PlayerBorder.cs
+++ Assets/Scripts/PlayerBorder.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerBorder : MonoBehaviour
 {
-    //public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +13,10 @@ public class PlayerBorder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //playerrb = GameObject.FindWithTag("Player").GetComponent<rb>;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        /*if (collision.gameObject.CompareTag("Finish"))
-        {
-            GameObject.FindWithTag("Player").GetComponent<Player>().rb.velocity = new Vector2(GameObject.FindWithTag("Player").GetComponent<Player>().rb.velocity.x + 1, GameObject.FindWithTag("Player").GetComponent<Player>().rb.velocity.y);
-        }*/
         if (collision.gameObject.CompareTag("Finish"))
         {
             GameObject.FindWithTag("Player").GetComponent<Player>().gameOver = true;
