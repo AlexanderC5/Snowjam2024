088c069d3a782ba7a03da4c5dc822234124d0c57
AlexanderC5 <91765309+AlexanderC5@users.noreply.github.com>
2024-01-04 14:06:06 -0800

    Cinemachine

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] public bool isEnabled;
+    [SerializeField] private float offsetX;
+    [SerializeField] private float offsetY;
     private GameObject player;
     private Vector2 playerPos;
 
@@ -21,7 +23,7 @@ public class FollowPlayer : MonoBehaviour
         if (isEnabled)
         {
             playerPos = new Vector2(player.transform.localPosition.x, player.transform.localPosition.y);
-            this.transform.localPosition = new Vector3(playerPos.x, playerPos.y, this.transform.localPosition.z);
+            this.transform.localPosition = new Vector3(playerPos.x + offsetX, playerPos.y + offsetY, this.transform.localPosition.z);
         }
     }
 }
